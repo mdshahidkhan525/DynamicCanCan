0d9429edd539fa36458c0f3a8a6bad7ab0879565
Shahid Khan <shkhan@bestpeers.com>
2021-11-03 15:42:30 +0530

    signup modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password,:role_id)}
       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :role_id)}
   end
 end
